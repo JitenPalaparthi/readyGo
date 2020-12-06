41b5472fb0237cb9d1fe4c409d174da8bc10eeb2
jiten <jitenp@outlook.com>
2020-12-07 00:09:03 +0530

    Implement Lint option and respective code for linting all go files

--- cmd/apply.go
+++ cmd/apply.go
@@ -1,20 +1,27 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"readyGo/boxops"
 	"readyGo/generate"
 	"readyGo/mapping"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/lint"
 )
 
 var applyFile, projectType string
+var lintFiles bool
 
 func init() {
 
 	applyCmd.Flags().StringVarP(&applyFile, "filename", "f", "default", "config file to generate the project")
 	applyCmd.Flags().StringVarP(&projectType, "type", "t", "http_mongo", "type of the project http_mongo | http_sql_pg | grpc_mogo | grpc_sql | cloudevent")
+	applyCmd.Flags().BoolVarP(&lintFiles, "lint", "l", false, "lints all generated files and gives warnings and errors")
 
 	rootCmd.AddCommand(applyCmd)
 }
@@ -27,7 +34,6 @@ var applyCmd = &cobra.Command{
 
 		ops := boxops.New("../box")
 		mapping, err := mapping.New(ops, "configs/mappings.json", projectType)
-		//fmt.Println(*mapping)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -44,6 +50,44 @@ var applyCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
+		fmt.Println(lintFiles)
+		if lintFiles {
+			err := filepath.Walk("./"+tg.Project,
+				func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					extension := filepath.Ext(path)
+					if !info.IsDir() && extension == ".go" {
+						lintFile(path, 0.2)
+					}
+					return nil
+				})
 
+			if err != nil {
+				log.Println(err)
+			}
+
+		}
 	},
+}
+
+func lintFile(filename string, minConfidence float64) {
+	src, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	l := new(lint.Linter)
+	ps, err := l.Lint(filename, src)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v:%v\n", filename, err)
+		return
+	}
+	for _, p := range ps {
+		if p.Confidence >= minConfidence {
+			fmt.Printf("%s:%v: %s\n", filename, p.Position, p.Text)
+		}
+	}
 }
