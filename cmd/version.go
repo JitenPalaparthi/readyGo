9e4ebdca0f51a810aa6589b9c6fb2c4045fd0368
jiten <jitenp@outlook.com>
2020-12-08 23:59:01 +0530

    Update version information

--- cmd/version.go
+++ cmd/version.go
@@ -12,9 +12,9 @@ func init() {
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "readyGo v0.1",
-	Long:  `readyGo v0.1.Currently in Alpha stage`,
+	Short: "readyGo v0.0.3",
+	Long:  `readyGo v0.0.3.Currently in Alpha stage`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("readyGo  v0.1")
+		fmt.Println("readyGo  v0.0.3")
 	},
 }
