b805140e9e3419610ddae966ae35955618e9adc8
jiten <jitenp@outlook.com>
2020-12-29 23:02:09 +0530

    Cleanup main.go

--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"log"
-	"readyGo/boxops"
 	"readyGo/cmd"
-	"readyGo/scaler"
 )
 
 func main() {
 	// Muruga bless me.
-
-	ops := boxops.New("../box")
-	m, err := scaler.New(ops, "configs/scalers.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(len(m))
-	log.Println(m.GetScaler("int").GoType)
 	cmd.Execute()
 }
