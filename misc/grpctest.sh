d8300ed536130846aaf194f88c14f44256f662f8
jiten <jitenp@outlook.com>
2020-12-29 22:30:19 +0530

    Update version information and other modifications

--- misc/grpctest.sh
+++ misc/grpctest.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
-rm -rf grpcsample
+rm -rf productsample
 
-go run main.go apply -f box/configs/config_grpc_mongo.json -t grpc_mongo
+go run main.go apply -f box/configs/config_grpc_mongo_product.json -t grpc_mongo
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsample/protos/address.proto
+#protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsample/protos/address.proto
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsample/protos/person.proto
+#protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsample/protos/person.proto
 
-cd grpcsample
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative productsample/protos/product.proto
 
-go mod init grpcsample
+cd productsample
+
+go mod init productsample
 
 go run main.go
 
