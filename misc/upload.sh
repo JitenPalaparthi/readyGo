50efb23519d89e5f2053c6f1307774baa7cbc989
jiten <jitenp@outlook.com>
2021-01-02 22:24:32 +0530

    Update upload.sh

--- misc/upload.sh
+++ misc/upload.sh
@@ -1,7 +1,5 @@
  #!/bin/sh
  # git tag -a v0.0.3 -m "new release v0.0.3"
-
-#export GITHUB_TOKEN="10cbdbac9fb8b7739efb74b896db60489dc30b9a"
-export GITHUB_TOKEN="fbe64eda4ea431deb3d13cc26b4ff5a1352ade0c"
+export GITHUB_TOKEN=""
 export TAG=v0.0.3
 ~/go/bin/ghr -t $GITHUB_TOKEN -u JitenPalaparthi -r readyGo --replace --draft  $TAG dist/
