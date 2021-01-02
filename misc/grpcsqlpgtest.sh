a7ddda9d9e6a789f09939fa8de19bae75b06fc6a
jiten <jitenp@outlook.com>
2021-01-03 00:25:37 +0530

    Add pg sql support for grpc

---
+++ misc/grpcsqlpgtest.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+rm -rf grpcsqlpgsample
+
+go run main.go apply -f misc/test_configs/config_grpc_sql_pg.json -t grpc_sql_pg
+
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsqlpgsample/protos/address.proto
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative grpcsqlpgsample/protos/product.proto
+
+cd grpcsqlpgsample
+
+go mod init grpcsqlpgsample
+
+go run main.go
