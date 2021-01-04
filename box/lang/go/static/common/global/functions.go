bd2b80e59a1121cd65d4641aaa1c17d59b4e2a8e
jiten <jitenp@outlook.com>
2021-01-05 00:01:06 +0530

    Add new global functions

--- box/lang/go/static/common/global/functions.go
+++ box/lang/go/static/common/global/functions.go
@@ -15,6 +15,21 @@ func GetCurrentDateTimeInTime() time.Time {
 }
 
 // GetDefaultStr is to get the default value which is of string type
-func GetDefaultStr(str string) string {
-	return str
+func GetDefaultStr(in string) string {
+	return in
+}
+
+// GetDefaultInt is to get the default integer value
+func GetDefaultInt(in int32) int32 {
+	return in
+}
+
+// GetDefaultLong is to get the default int64 value
+func GetDefaultLong(in int64) int64 {
+	return in
+}
+
+// GetDefaultBool is to get the default bool value
+func GetDefaultBool(in bool) bool {
+	return in
 }
