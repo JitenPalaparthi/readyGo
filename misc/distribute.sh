ec075dc9b4453b2a1e445cb8133b0e129175e486
jiten <jitenp@outlook.com>
2021-04-23 16:26:56 +0530

    Add examples directory for configurations

--- misc/distribute.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/sh
-
- # ~/go/bin/gox -os="linux darwin windows" -arch="amd64" -output="dist/readyGo_{{.OS}}_{{.Arch}}"
-
-~/go/bin/gox -output="dist/readyGo_v0.0.3_{{.OS}}_{{.Arch}}"
-
-# For all os 
-
- cd dist;gzip *
